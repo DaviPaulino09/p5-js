72dd450d6abedefd203d156bec05919424b981a8
DaviPaulino09 <00001126768005sp@al.educacao.sp.gov.br>
2024-08-06 17:03:19 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(600,400);
+  }
+  
+  function draw() {
+    background("#DA00FF");
+    stroke("#37CC74")
+    strokeWeight("20")
+   fill("#FF0000") 
+    rect (mouseX,mouseY,120,120)
+  }
